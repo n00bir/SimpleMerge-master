a48ce89a0c1155679ef6c65393cc065f670ccb01
BT_1N3_13@bktech.local <BT_1N3_13@bktech.local>
2019-02-11 10:55:52 -0500

    Initial commit

---
+++ src/SimpleMerge.java
@@ -0,0 +1,40 @@
+/**
+ * Created by Teacher on 1/28/2019.
+ * simpleMerge takes in two sorted arrays of
+ * integers and merges them to return one sorted
+ * array of integers.
+ */
+public class SimpleMerge {
+    public static int[] simpleMerge(int[] arr1, int[] arr2) {
+        int i = 0, j = 0;
+        boolean done = false;
+        int[] output = new int[arr1.length + arr2.length];
+        while (!done) {
+            if (arr1[i] < arr2[j]) {
+                output[i + j] = arr1[i];
+                i ++;
+            }
+            else {
+                output[i + j] = arr2[j];
+                j ++;
+            }
+
+            if (j == arr2.length - 1) {
+                for (i = i; i < arr1.length; i ++) {
+                    output[i + j] = arr1[i];
+                }
+                done = true;
+            }
+
+            if (i == arr1.length - 1) {
+                for (j = j; j < arr2.length; j ++) {
+                    output[i + j] = arr2[j];
+                }
+                done = true;
+            }
+
+        }
+        return output;
+    }
+
+}
